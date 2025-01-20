f40e73017e9591ac235de832c36050831dc2fb8c
227054 <227054@KB37-115-C13>
2025-01-20 14:24:39 +0300

    ПР №1 часть 1 завершена

--- 122_Zyamilov/MainWindow.xaml.cs
+++ 122_Zyamilov/MainWindow.xaml.cs
@@ -24,5 +24,79 @@ namespace _122_Zyamilov
         {
             InitializeComponent();
         }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(InputX.Text))
+                {
+                    MessageBox.Show("Введите значение x!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (!double.TryParse(InputX.Text, out double x))
+                {
+                    MessageBox.Show("Некорректное значение x! Введите число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (string.IsNullOrWhiteSpace(InputY.Text))
+                {
+                    MessageBox.Show("Введите значение y!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                if (!double.TryParse(InputX.Text, out double y))
+                {
+                    MessageBox.Show("Некорректное значение y! Введите число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                double f_x = 0;
+                if (RadioSh.IsChecked == true)
+                {
+                    f_x = Math.Sinh(x);
+                }
+                else if (RadioSquare.IsChecked == true) { 
+                    f_x = Math.Pow(x, 2);
+                }
+                else if (RadioExp.IsChecked == true)
+                {
+                    f_x = Math.Exp(x);
+                }
+
+                double d;
+
+                if (x > y)
+                {
+                    d = Math.Pow(f_x-y, 3) + Math.Atan(f_x);
+                }
+                else if (y > x)
+                {
+                    d = Math.Pow(y - f_x, 3) + Math.Atan(f_x);
+                }
+                else
+                {
+                    d = Math.Pow(y + f_x, 3) + 0.5;
+                }
+
+                ResultBox.Text = d.ToString("F2");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+           
+
+
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputX.Clear();
+            InputY.Clear();
+            RadioSh.IsChecked = true;
+            ResultBox.Clear();
+        }
     }
 }
